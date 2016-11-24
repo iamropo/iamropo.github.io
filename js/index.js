e241a466de67ab8a1aed1ba1704c1cf71d64fde0
iamropo <iamropo@gmail.com>
2016-11-24 20:28:00 +0530

    subu nomo's website

--- js/index.js
+++ js/index.js
@@ -1,18 +1,31 @@
 var music = document.querySelector('#music')
 var video = document.querySelector('#video')
-var activeColor = '#fafafa'
+var bio = document.querySelector('#bio')
+
+var activeColor = '#e0e0e0'
 var inactiveColor = '#9e9e9e'
+
 var musicContainer = document.querySelector('#music-container')
 var videoContainer = document.querySelector('#video-container')
+var bioContainer = document.querySelector('#bio-container')
 
-function navigate (target, victim, targetContainer, victimContainer) {
-	target.addEventListener('click', () => {
-		target.style.color = activeColor
+function hide (victims, victimContainers) {
+	victims.forEach((victim) => {
 		victim.style.color = inactiveColor
-		victimContainer.style.display = 'none'
+	})
+	victimContainers.forEach((container) => {
+		container.style.display = 'none'
+	})
+}
+
+function navigate (target, victims, targetContainer, victimContainers) {
+	target.addEventListener('click', () => {
+		hide(victims, victimContainers)
+		target.style.color = activeColor
 		targetContainer.style.display = 'block'
 	})
 }
 
-navigate(music, video, musicContainer, videoContainer)
-navigate(video, music, videoContainer, musicContainer)
+navigate(music, [video, bio], musicContainer, [videoContainer, bioContainer])
+navigate(video, [music, bio], videoContainer, [musicContainer, bioContainer])
+navigate(bio, [video, music], bioContainer, [videoContainer, musicContainer])
