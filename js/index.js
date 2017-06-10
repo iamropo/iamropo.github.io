44a9230114b5d96b2607c749b4ea36007f729015
iamropo <iamropo@gmail.com>
2017-06-10 20:17:33 +0530

    subu nomo ka website

---
+++ js/index.js
@@ -0,0 +1,36 @@
+// Year:
+document.querySelector('#current-year').innerText = new Date().getFullYear()
+// Navigation:
+var music = document.querySelector('#music')
+var discography = document.querySelector('#discography')
+var bio = document.querySelector('#bio')
+
+var activeColor = '#efefef'
+var inactiveColor = '#9e9e9e'
+
+var musicContainer = document.querySelector('#music-container')
+var discographyContainer = document.querySelector('#discography-container')
+var bioContainer = document.querySelector('#bio-container')
+
+function hide (victims, victimContainers) {
+	victims.forEach((victim) => {
+		victim.style.color = inactiveColor
+		victim.style.border = 'none'
+	})
+	victimContainers.forEach((container) => {
+		container.style.display = 'none'
+	})
+}
+
+function navigate (target, victims, targetContainer, victimContainers) {
+	target.addEventListener('click', () => {
+		hide(victims, victimContainers)
+		target.style.color = activeColor
+		target.style.borderBottom = '3px solid ' + activeColor
+		targetContainer.style.display = 'block'
+	})
+}
+
+navigate(music, [discography, bio], musicContainer, [discographyContainer, bioContainer])
+navigate(discography, [music, bio], discographyContainer, [musicContainer, bioContainer])
+navigate(bio, [discography, music], bioContainer, [discographyContainer, musicContainer])
