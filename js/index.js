93e8c1bfb10b82d440a07170347eac0fc29f89ce
iamropo <iamropo@gmail.com>
2017-06-10 23:45:14 +0530

    added contacts section

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ document.querySelector('#current-year').innerText = new Date().getFullYear()
 var music = document.querySelector('#music')
 var discography = document.querySelector('#discography')
 var bio = document.querySelector('#bio')
+var contact = document.querySelector('#contact')
 
 var activeColor = '#efefef'
 var inactiveColor = '#9e9e9e'
@@ -11,6 +12,7 @@ var inactiveColor = '#9e9e9e'
 var musicContainer = document.querySelector('#music-container')
 var discographyContainer = document.querySelector('#discography-container')
 var bioContainer = document.querySelector('#bio-container')
+var contactDetailsContainer = document.querySelector('#contact-details-container')
 
 function hide (victims, victimContainers) {
 	victims.forEach((victim) => {
@@ -31,6 +33,7 @@ function navigate (target, victims, targetContainer, victimContainers) {
 	})
 }
 
-navigate(music, [discography, bio], musicContainer, [discographyContainer, bioContainer])
-navigate(discography, [music, bio], discographyContainer, [musicContainer, bioContainer])
-navigate(bio, [discography, music], bioContainer, [discographyContainer, musicContainer])
+navigate(music, [discography, bio, contact], musicContainer, [discographyContainer, bioContainer, contactDetailsContainer])
+navigate(discography, [music, bio, contact], discographyContainer, [musicContainer, bioContainer, contactDetailsContainer])
+navigate(bio, [discography, music, contact], bioContainer, [discographyContainer, musicContainer, contactDetailsContainer])
+navigate(contact, [music, discography, bio], contactDetailsContainer, [musicContainer, discographyContainer, bioContainer])
